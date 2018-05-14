1d3cbd2e3e09903da9bd48437ee9fa9ef800e2af
GTimur <to-timur@yandex.ru>
2018-05-14 18:00:28 +0300

    task

--- main.go
+++ main.go
@@ -4,37 +4,11 @@ import (
 	"fmt"
 	"log"
 	"memogo"
-	"time"
 )
-
-// REMIND SCENARIO
-// REPEAT_UNTIL_EVENT_START = Remind in Int1 before event DateStart and repeat remind every Int2 until event DateStart
-// REPEAT_UNTIL_EVENT_END = Remind from DateStart to DateEnd with repeat inteval=Int2 (minutes)
-
-// Remind options
-type Remind struct {
-	DateStart time.Time //Date start of event
-	DateEnd   time.Time //Date end of event
-	Int1      int       //Frequency of reminder messages (minutes)
-	Int2      int       //Frequency of reminder messages (minutes)
-}
-
-// Memo definiton
-type Memo struct {
-	ID       int       //ID
-	Date     time.Time //Date of creation
-	Reminder Remind    //Remind scenario options
-	Subj     string    //visible memo subject
-	Memo     []string  //memo body
-	Mails    []string  //emails list
-}
 
 var globalconfig memogo.Config
 
 func main() {
-	memo := Memo{}
-	rem := Remind{}
-	//smtp :=
 	globalconfig = memogo.Config{
 		Root: "./root/",
 		SMTPSrv: memogo.SrvSMTP{
@@ -54,26 +28,7 @@ func main() {
 
 	var files map[string]string
 
-	loc, err := time.LoadLocation("Europe/Moscow")
-	if err != nil {
-		panic(err)
-	}
-
-	rem.DateStart = time.Date(2018, time.May, 11, 10, 0, 0, 0, loc)
-	rem.DateEnd = time.Date(2018, time.August, 16, 23, 59, 59, 0, loc)
-	rem.Int1 = 5 //in * minutes
-	rem.Int2 = 1 //every * minutes
-
-	memo.ID = 100
-	memo.Date = time.Now()
-	memo.Subj = "Сертификат thawte ibank.ymkbank.ru истекает 17.08.2018"
-	memo.Memo = []string{"Истекает срок действия сертификата для", "web-сервера ibank.ymkbank.ru"}
-	memo.Mails = []string{"gtg@ymkbank.ru"}
-	memo.Reminder = rem
-
-	fmt.Println(memo)
-
-	files, err = memogo.FindAllFiles(globalconfig.Root, []string{"*.*"})
+	files, err := memogo.FindAllFiles(globalconfig.Root, []string{"*.*"})
 	if err != nil {
 		log.Fatalf("Main(): FindFiles error: %v", err)
 	}
@@ -87,5 +42,10 @@ func main() {
 	err = globalconfig.WriteJSON()
 	if err != nil {
 		panic(err)
+	}
+
+	err = memogo.TestJSON()
+	if err != nil {
+		log.Fatalf("TestJSON() error: %v", err)
 	}
 }
